2b94cfd635a9c1c4934b0cbf5e3df9ce276a357a
KrokaKrola <harlamsan@gmail.com>
2026-06-09 16:24:04 +0300

    codecrafters submit [skip ci]

--- app/bit_torrent.go
+++ app/bit_torrent.go
@@ -1,19 +1,36 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"net/url"
 	"os"
+	"slices"
+	"strconv"
 )
 
-type bitTorrentFile struct {
-	announce string
-	info     dictionary
+type bitTorrentFileInfo struct {
+	length      int
+	pieceLength int
+	pieces      string
+	piecesParts [][]byte
 }
 
-func (btf *bitTorrentFile) parse(fileName string) error {
+type bitTorrentFile struct {
+	announce    string
+	shaInfoHash string
+	infoRaw     dictionary
+	info        *bitTorrentFileInfo
+	peers       []string
+}
+
+func parseBitTorrentFile(fileName string) (*bitTorrentFile, error) {
 	data, err := os.ReadFile(fileName)
 	if err != nil {
-		return fmt.Errorf("Error while trying to read file: %s", err.Error())
+		return nil, fmt.Errorf("Error while trying to read file: %s", err.Error())
 	}
 
 	dec := decoder{
@@ -25,21 +42,239 @@ func (btf *bitTorrentFile) parse(fileName string) error {
 	if res, ok := dec.decode().(dictionary); ok {
 		dict = res
 	} else {
-		return fmt.Errorf("Invalid file content")
+		return nil, fmt.Errorf("Invalid file content")
 	}
 
 	announce, ok := findElementInDictionary[string](dict, "announce")
 	if !ok {
-		return fmt.Errorf("Invalid file content, announce field was not found")
+		return nil, fmt.Errorf("Invalid file content, announce field was not found")
 	}
 
 	info, ok := findElementInDictionary[dictionary](dict, "info")
 	if !ok {
-		return fmt.Errorf("Invalid file content, info field was not found")
+		return nil, fmt.Errorf("Invalid file content, info field was not found")
 	}
 
-	btf.announce = announce
-	btf.info = info
+	encodedInfo, err := encode(info)
+	if err != nil {
+		fmt.Printf("Error while trying to encode info content: %s", err.Error())
+		os.Exit(1)
+	}
+
+	hash, err := createSha1HashFromString(encodedInfo, false)
+	if err != nil {
+		fmt.Println("Error while trying to create hash from info content")
+		os.Exit(1)
+	}
+
+	infoLength, ok := findElementInDictionary[int](info, "length")
+	if !ok {
+		fmt.Println("Length field in info is not found")
+		os.Exit(1)
+	}
+
+	pieceLength, ok := findElementInDictionary[int](info, "piece length")
+	if !ok {
+		fmt.Println("Piece length filed in info is not found")
+		os.Exit(1)
+	}
+
+	pieces, ok := findElementInDictionary[string](info, "pieces")
+	if !ok {
+		fmt.Println("Pieces field is not found")
+		os.Exit(1)
+	}
+
+	var piecesParts [][]byte
+
+	for i := 0; i < len(pieces); i += 20 {
+		piecesParts = append(piecesParts, []byte(pieces[i:i+20]))
+	}
+
+	return &bitTorrentFile{
+		announce:    announce,
+		infoRaw:     info,
+		shaInfoHash: hash,
+		info: &bitTorrentFileInfo{
+			length:      infoLength,
+			pieceLength: pieceLength,
+			pieces:      pieces,
+			piecesParts: piecesParts,
+		},
+	}, nil
+}
+
+func enrichWithPeers(btFile *bitTorrentFile) error {
+
+	requestUrl, err := url.Parse(btFile.announce)
+	if err != nil {
+		return fmt.Errorf("Error while parsing announce URL: %s\n", err.Error())
+	}
+
+	requestQuery := requestUrl.Query()
+	requestQuery.Set("info_hash", btFile.shaInfoHash)
+	requestQuery.Set("peer_id", generateId())
+	requestQuery.Set("port", "6881")
+	requestQuery.Set("uploaded", "0")
+	requestQuery.Set("downloaded", "0")
+	requestQuery.Set("left", strconv.Itoa(btFile.info.length))
+	requestQuery.Set("compact", "1")
+
+	requestUrl.RawQuery = requestQuery.Encode()
+
+	client := &http.Client{}
+	request, err := http.NewRequest(http.MethodGet, requestUrl.String(), nil)
+	if err != nil {
+		return fmt.Errorf("Error during request to announce URL: %s\n", err.Error())
+	}
+	response, err := client.Do(request)
+	if err != nil {
+		return fmt.Errorf("Error during request to announce URL: %s\n", err.Error())
+	}
+
+	body, err := io.ReadAll(response.Body)
+	defer response.Body.Close()
+
+	bodyDecoder := &decoder{data: body}
+	res := bodyDecoder.decode()
+
+	dict, ok := res.(dictionary)
+	if !ok {
+		return fmt.Errorf("Invalid response from announce URL")
+	}
+
+	peers, ok := findElementInDictionary[string](dict, "peers")
+	if !ok {
+		return fmt.Errorf("Invalid response from announce URL, peers field is not in the response")
+	}
+
+	peersAsBytesArr := []byte(peers)
+	var result []string
+
+	for i := 0; i < len(peersAsBytesArr); i += 6 {
+		ip1 := peersAsBytesArr[i]
+		ip2 := peersAsBytesArr[i+1]
+		ip3 := peersAsBytesArr[i+2]
+		ip4 := peersAsBytesArr[i+3]
+		port := binary.BigEndian.Uint16(peersAsBytesArr[i+4 : i+6])
+
+		result = append(result, fmt.Sprintf("%d.%d.%d.%d:%d", ip1, ip2, ip3, ip4, port))
+	}
+
+	btFile.peers = result
 
 	return nil
+}
+
+type peer struct {
+	id   []byte
+	conn net.Conn
+}
+
+func handshakeWithPeer(btFile *bitTorrentFile, address string) (*peer, error) {
+	fmt.Println("handshake with", address)
+	conn, err := net.Dial("tcp", address)
+	if err != nil {
+		return nil, fmt.Errorf("Error while trying to establish TCP connection to: %s. Error: %s", address, err.Error())
+	}
+
+	message := []byte{19}
+	reserved := make([]byte, 8)
+
+	message = append(message, []byte("BitTorrent protocol")...)
+	message = append(message, reserved...)
+	message = append(message, []byte(btFile.shaInfoHash)...)
+	message = append(message, []byte(generateId())...)
+
+	_, err = conn.Write(message)
+	if err != nil {
+		conn.Close()
+		return nil, fmt.Errorf("Error while trying to send message to TCP connection: %s", err.Error())
+	}
+
+	res := make([]byte, 68)
+	if _, err := io.ReadFull(conn, res); err != nil {
+		conn.Close()
+		return nil, fmt.Errorf("Error while trying to read message from TCP connection: %s", err.Error())
+	}
+
+	return &peer{
+		id:   res[48:],
+		conn: conn,
+	}, nil
+}
+
+const infoBlockSize = 16384 // 16 kiB (16 * 1024 bytes)
+
+func getPiece(btFile *bitTorrentFile, peer *peer, pieceIndex int) ([]byte, error) {
+	bitfieldMsgSizeBuff := make([]byte, 4)
+	if _, err := io.ReadFull(peer.conn, bitfieldMsgSizeBuff); err != nil {
+		return nil, fmt.Errorf("error reading bitfield msg size from peer")
+	}
+
+	bitfieldMsg := make([]byte, binary.BigEndian.Uint32(bitfieldMsgSizeBuff))
+	if _, err := io.ReadFull(peer.conn, bitfieldMsg); err != nil {
+		fmt.Println("error reading bitfield msg from peer")
+	}
+
+	intrestedMsg := []byte{}
+	intrestedMsg = append(intrestedMsg, binary.BigEndian.AppendUint32(nil, 1)...)
+	intrestedMsg = append(intrestedMsg, []byte{2}...)
+
+	if _, err := peer.conn.Write(intrestedMsg); err != nil {
+		return nil, fmt.Errorf("error writing intrested msg to peer")
+	}
+
+	unchokeMsgSizeBuff := make([]byte, 4)
+	if _, err := io.ReadFull(peer.conn, unchokeMsgSizeBuff); err != nil {
+		return nil, fmt.Errorf("error reading unchoke msg size from peer")
+	}
+
+	unchokeMsg := make([]byte, binary.BigEndian.Uint32(unchokeMsgSizeBuff))
+	if _, err := io.ReadFull(peer.conn, unchokeMsg); err != nil {
+		return nil, fmt.Errorf("error reading unchoke msg from peer")
+	}
+
+	blocks := [][]byte{}
+
+	// last piece may be smaller than pieceLength if file size is not a multiple of pieceLength
+	actualPieceLength := btFile.info.pieceLength
+	if pieceIndex == len(btFile.info.piecesParts)-1 {
+		actualPieceLength = btFile.info.length - (pieceIndex * btFile.info.pieceLength)
+	}
+
+	for begin := uint32(0); begin < uint32(actualPieceLength); begin += infoBlockSize {
+		length := min(infoBlockSize, uint32(actualPieceLength)-begin)
+
+		requestMsg := []byte{}
+
+		requestMsg = append(requestMsg, binary.BigEndian.AppendUint32(nil, 13)...)
+		requestMsg = append(requestMsg, byte(6))
+		requestMsg = append(requestMsg, binary.BigEndian.AppendUint32(nil, uint32(pieceIndex))...)
+		requestMsg = append(requestMsg, binary.BigEndian.AppendUint32(nil, begin)...)
+		requestMsg = append(requestMsg, binary.BigEndian.AppendUint32(nil, length)...)
+
+		if _, err := peer.conn.Write(requestMsg); err != nil {
+			return nil, fmt.Errorf("error writing request msg to peer")
+		}
+
+		pieceMsgBuffSize := make([]byte, 4)
+		if _, err := io.ReadFull(peer.conn, pieceMsgBuffSize); err != nil {
+			return nil, fmt.Errorf("error reading msg size for block request")
+		}
+
+		msgLen := binary.BigEndian.Uint32(pieceMsgBuffSize)
+
+		pieceMsg := make([]byte, msgLen)
+		if _, err := io.ReadFull(peer.conn, pieceMsg); err != nil {
+			return nil, fmt.Errorf("error reading block msg from peer")
+		}
+
+		block := pieceMsg[9:]
+		blocks = append(blocks, block)
+	}
+
+	piece := slices.Concat(blocks...)
+
+	return piece, nil
 }
--- app/main.go
+++ app/main.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"encoding/binary"
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
-	"io"
-	"net"
-	"net/http"
-	"net/url"
 	"os"
 	"strconv"
 	// bencode "github.com/jackpal/bencode-go" // Available if you need it!
@@ -23,6 +18,77 @@ func main() {
 	command := os.Args[1]
 
 	switch command {
+	case "download_piece":
+		// ./your_program.sh download_piece -o /tmp/test-piece sample.torrent <piece_index>
+		if len(os.Args) < 6 {
+			fmt.Println("Invalid number of arguments")
+			os.Exit(1)
+		}
+
+		fileName := os.Args[4]
+
+		if fileName == "" {
+			fmt.Println("Empty file name")
+			os.Exit(1)
+		}
+
+		pieceIndexStr := os.Args[5]
+		if pieceIndexStr == "" {
+			fmt.Println("piece_index is empty")
+			os.Exit(1)
+		}
+
+		pieceIndex, err := strconv.Atoi(pieceIndexStr)
+		if err != nil {
+			fmt.Println("piece_index is not a number")
+			os.Exit(1)
+		}
+
+		btFile, err := parseBitTorrentFile(fileName)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		if err = enrichWithPeers(btFile); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		if len(btFile.peers) == 0 {
+			fmt.Println("no peers for torrent file", fileName)
+			os.Exit(1)
+		}
+
+		peer, err := handshakeWithPeer(btFile, btFile.peers[0])
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		defer peer.conn.Close()
+
+		piece, err := getPiece(btFile, peer, pieceIndex)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		pieceHash, err := createSha1Hash(piece, true)
+		if err != nil {
+			fmt.Println("error creating hash for received blocks")
+			os.Exit(1)
+		}
+
+		if pieceHash != hex.EncodeToString(btFile.info.piecesParts[pieceIndex]) {
+			fmt.Println("piece hash is invalid")
+			os.Exit(1)
+		}
+
+		if err := os.WriteFile(os.Args[3], piece, 0644); err != nil {
+			fmt.Println("error saving the file into", os.Args[3])
+			os.Exit(1)
+		}
 	case "handshake":
 		if len(os.Args) < 3 {
 			fmt.Println("Invalid number of arguments")
@@ -30,8 +96,6 @@ func main() {
 		}
 		fileName := os.Args[2]
 		peerAddress := os.Args[3]
-
-		fmt.Println(peerAddress)
 
 		if fileName == "" {
 			fmt.Println("Empty file name")
@@ -43,53 +107,20 @@ func main() {
 			os.Exit(1)
 		}
 
-		btFile := &bitTorrentFile{}
-		err := btFile.parse(fileName)
+		btFile, err := parseBitTorrentFile(fileName)
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
 
-		conn, err := net.Dial("tcp", peerAddress)
+		peer, err := handshakeWithPeer(btFile, peerAddress)
 		if err != nil {
-			fmt.Println("Error while trying to establish TCP connection to", peerAddress)
+			fmt.Println(err)
 			os.Exit(1)
 		}
+		defer peer.conn.Close()
 
-		defer conn.Close()
-
-		encodedInfo, err := encode(btFile.info)
-		if err != nil {
-			fmt.Printf("Error while trying to encode info content: %s", err.Error())
-			os.Exit(1)
-		}
-
-		infoHash, err := createSha1HashFromString(encodedInfo, false)
-		if err != nil {
-			fmt.Println("Error while trying to create hash from info content")
-			os.Exit(1)
-		}
-
-		message := []byte{19}
-		reserved := make([]byte, 8)
-
-		message = append(message, []byte("BitTorrent protocol")...)
-		message = append(message, reserved...)
-		message = append(message, []byte(infoHash)...)
-		message = append(message, []byte(generateId())...)
-
-		_, err = conn.Write(message)
-		if err != nil {
-			fmt.Println("Error while trying to send message to TCP connection:", err.Error())
-			os.Exit(1)
-		}
-
-		res, err := io.ReadAll(conn)
-		if err != nil {
-			fmt.Println("Error while trying to read message from TCP connection:", err.Error())
-		}
-
-		fmt.Println("Peer ID:", hex.EncodeToString(res[48:]))
+		fmt.Println("Peer ID:", hex.EncodeToString(peer.id))
 	case "peers":
 		fileName := os.Args[2]
 
@@ -98,89 +129,19 @@ func main() {
 			os.Exit(1)
 		}
 
-		btFile := &bitTorrentFile{}
-		err := btFile.parse(fileName)
+		btFile, err := parseBitTorrentFile(fileName)
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
 
-		encodedInfo, err := encode(btFile.info)
-		if err != nil {
-			fmt.Printf("Error while trying to encode info content: %s", err.Error())
-			os.Exit(1)
-		}
-
-		infoHash, err := createSha1HashFromString(encodedInfo, false)
-		if err != nil {
-			fmt.Println("Error while trying to create hash from info content")
-			os.Exit(1)
-		}
-
-		infoLength, ok := findElementInDictionary[int](btFile.info, "length")
-		if !ok {
-			fmt.Println("Length field in info is not found")
-			os.Exit(1)
-		}
-
-		requestUrl, err := url.Parse(btFile.announce)
-		if err != nil {
-			fmt.Printf("Error while parsing announce URL: %s\n", err.Error())
-			os.Exit(1)
-		}
-
-		requestQuery := requestUrl.Query()
-		requestQuery.Set("info_hash", string(infoHash))
-		requestQuery.Set("peer_id", generateId())
-		requestQuery.Set("port", "6881")
-		requestQuery.Set("uploaded", "0")
-		requestQuery.Set("downloaded", "0")
-		requestQuery.Set("left", strconv.Itoa(infoLength))
-		requestQuery.Set("compact", "1")
-
-		requestUrl.RawQuery = requestQuery.Encode()
-
-		client := &http.Client{}
-		request, err := http.NewRequest(http.MethodGet, requestUrl.String(), nil)
-		if err != nil {
+		if err = enrichWithPeers(btFile); err != nil {
 			fmt.Println(err)
-			fmt.Printf("Error during request to announce URL: %s\n", err.Error())
-			os.Exit(1)
-		}
-		response, err := client.Do(request)
-		if err != nil {
-			fmt.Printf("Error during request to announce URL: %s\n", err.Error())
 			os.Exit(1)
 		}
 
-		body, err := io.ReadAll(response.Body)
-		defer response.Body.Close()
-
-		bodyDecoder := &decoder{data: body}
-		res := bodyDecoder.decode()
-
-		dict, ok := res.(dictionary)
-		if !ok {
-			fmt.Println("Invalid response from announce URL")
-			os.Exit(1)
-		}
-
-		peers, ok := findElementInDictionary[string](dict, "peers")
-		if !ok {
-			fmt.Println("Invalid response from announce URL, peers field is not in the response")
-			os.Exit(1)
-		}
-
-		peersAsBytesArr := []byte(peers)
-
-		for i := 0; i < len(peersAsBytesArr); i += 6 {
-			ip1 := peersAsBytesArr[i]
-			ip2 := peersAsBytesArr[i+1]
-			ip3 := peersAsBytesArr[i+2]
-			ip4 := peersAsBytesArr[i+3]
-			port := binary.BigEndian.Uint16(peersAsBytesArr[i+4 : i+6])
-
-			fmt.Printf("%d.%d.%d.%d:%d\n", ip1, ip2, ip3, ip4, port)
+		for _, el := range btFile.peers {
+			fmt.Println(el)
 		}
 	case "info":
 		fileName := os.Args[2]
@@ -190,56 +151,19 @@ func main() {
 			os.Exit(1)
 		}
 
-		btFile := &bitTorrentFile{}
-		err := btFile.parse(fileName)
+		btFile, err := parseBitTorrentFile(fileName)
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
 
-		var encodedPieces []string
-
-		pieces, ok := findElementInDictionary[string](btFile.info, "pieces")
-		if !ok {
-			fmt.Println("Pieces field is not found")
-			os.Exit(1)
-		}
-
-		for i := 0; i < len(pieces); i += 20 {
-			encodedPieces = append(encodedPieces, hex.EncodeToString([]byte(pieces[i:i+20])))
-		}
-
-		encodedInfo, err := encode(btFile.info)
-		if err != nil {
-			fmt.Printf("Error while trying to encode info content: %s", err.Error())
-			os.Exit(1)
-		}
-
-		hash, err := createSha1HashFromString(encodedInfo, true)
-		if err != nil {
-			fmt.Println("Error while trying to create hash from info content")
-			os.Exit(1)
-		}
-
-		infoLength, ok := findElementInDictionary[int](btFile.info, "length")
-		if !ok {
-			fmt.Println("Length field in info is not found")
-			os.Exit(1)
-		}
-
-		pieceLength, ok := findElementInDictionary[int](btFile.info, "piece length")
-		if !ok {
-			fmt.Println("Piece length filed in info is not found")
-			os.Exit(1)
-		}
-
 		fmt.Println("Tracker URL:", btFile.announce)
-		fmt.Println("Length:", infoLength)
-		fmt.Println("Info Hash:", hash)
-		fmt.Println("Piece Length:", pieceLength)
+		fmt.Println("Length:", btFile.info.length)
+		fmt.Println("Info Hash:", hex.EncodeToString([]byte(btFile.shaInfoHash)))
+		fmt.Println("Piece Length:", btFile.info.pieceLength)
 		fmt.Println("Piece Hashes:")
-		for _, el := range encodedPieces {
-			fmt.Println(el)
+		for _, el := range btFile.info.piecesParts {
+			fmt.Println(hex.EncodeToString(el))
 		}
 	case "decode":
 		bencodedValue := os.Args[2]
